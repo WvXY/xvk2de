9e945276ae678e4eddb8d85126a86ead0c74d5d1
xy-wu <wxy@g.ecc.u-tokyo.ac.jp>
2024-05-29 20:32:54 +0900

    remove duplicated code

--- src/components.hpp
+++ src/components.hpp
@@ -3,16 +3,6 @@
 #include "xev_aliases.hpp"
 
 namespace xev {
-class MotionComponent {
-public:
-  vec2 getPosition() const { return pos; }
-  vec2 getVelocity() const { return vel; }
-  vec2 getAcceleration() const { return acc; }
-  // TODO: use template
 
-private:
-  vec2 pos{0.f};
-  vec2 vel{0.f};
-  vec2 acc{0.f};
-};
+
 } // namespace xev
