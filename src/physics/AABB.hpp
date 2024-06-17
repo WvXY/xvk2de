720450f76fa97a78e97817d8893fe2835b7425b0
xy-wu <xy-wu@outlook.com>
2024-06-17 19:33:58 +0900

    aabb + operator

--- src/physics/AABB.hpp
+++ src/physics/AABB.hpp
@@ -10,6 +10,13 @@ struct AABB {
   AABB() = default;
   AABB(const T& min, const T& max) : min(min), max(max) {}
 
+  AABB operator+(const AABB& other) const {
+    AABB result;
+    result.max = glm::max(this->max, other.max);
+    result.min = glm::min(this->min, other.min);
+    return result;
+  }
+
   T min{};
   T max{};
 };
