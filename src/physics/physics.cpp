9237e4158e294c40ac35e83821a293d61620de07
xy-wu <xy-wu@outlook.com>
2024-06-19 09:55:05 +0900

    add headers for physics

--- src/physics/physics.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by xy-wu on 26-May-24.
-//
-
-#include "physics.hpp"
---
+++ src/physics/xe_collider.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace xev {
+
+struct BoxCollider {};
+
+struct CircleCollider{};
+
+struct PolygonCollider{};
+
+struct LineCollider{};
+
+
+}
+
